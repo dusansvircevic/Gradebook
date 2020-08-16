aec74255426349c5d13ad13aab987d2fec25352e
dusansvircevic <dusan.svircevic@gmail.com>
2020-08-16 17:23:09 +0200

    final

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,25 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+
+
+  Cypress.Commands.add('loginBackend', (mail, pass) => {
+    cy.request({
+        method: 'POST',
+        url: Cypress.env('apiUrl') + '/login',
+        form: true,
+        followRedirect: true,
+        body: {
+            email: mail,
+            password: pass,
+        }
+    }).
+        then((resp) => {
+            expect(resp.body).to.have.property('token')
+            localStorage.setItem('user_id', resp.body.user_id)
+            localStorage.setItem('token', resp.body.access_token)
+                 cy.visit('/')
+        })
+})
+
